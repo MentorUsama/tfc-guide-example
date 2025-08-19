2825ce2678bc4838e85d7171328c4f60e7e4692f
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2025-08-19 14:12:33 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,18 +1,9 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.1"
     }
   }
-
-  required_version = ">= 0.14.0"
 }
