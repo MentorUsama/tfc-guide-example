993ddac59575f284ea2cc4325b503bbc7796cc72
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2025-08-20 12:01:32 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "delay_seconds" { 
+  type = number 
+  description = "Delay applied during apply and destroy." default = 10 
+}
+
 variable "force_fail" {
   type        = bool
   description = "Set to true to force an error during plan (for testing)."
