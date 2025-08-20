e8456ac4308838b3e916de7c7a40b1c1b978e57b
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2025-08-20 12:02:04 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,8 +8,3 @@ variable "force_fail" {
   description = "Set to true to force an error during plan (for testing)."
   default     = true
 }
-
-locals {
-  # If force_fail = true, this triggers a plan-time error
-  intentional_error = var.force_fail ? file("nonexistent.file") : ""
-}
