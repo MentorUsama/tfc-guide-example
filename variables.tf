cfec13beaae7d89ce3f743ac1c73369d6bc5fb9a
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2025-08-20 12:00:48 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
-variable "delay_seconds" {
-  type        = number
-  description = "Delay applied during apply and destroy."
-  default     = 10
+variable "force_fail" {
+  type        = bool
+  description = "Set to true to force an error during plan (for testing)."
+  default     = true
+}
+
+locals {
+  # If force_fail = true, this triggers a plan-time error
+  intentional_error = var.force_fail ? file("nonexistent.file") : ""
 }
